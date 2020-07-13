d10d7f325f7fdf510eb6d486ecd99477220d30d4
Brett Andrews <brettandrews3@gmail.com>
2020-07-13 15:35:40 -0500

    Cleaned up comments for clarity. Ready to run

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -3,10 +3,6 @@ using System.Linq;
 using System.IO;
 using GeoCoordinatePortable;
 
-/* 1. Start with your unit tests
-2. Next, complete the Parse() method
-3. Complete Program.cs */
-
 namespace LoggingKata
 {
     class Program
@@ -16,8 +12,8 @@ namespace LoggingKata
 
         static void Main(string[] args)
         {
-            // TODO:  Find the two Taco Bells that are the furthest from one another.
-            // HINT:  You'll need two nested forloops ---------------------------
+            // Goal:  Find the two Taco Bells that are the furthest from one another.
+            // Tip:  You'll need two nested forloops ---------------------------
 
             logger.LogInfo("Log initialized");
 
@@ -33,11 +29,8 @@ namespace LoggingKata
             // Grab an IEnumerable of locations using the Select command: var locations = lines.Select(parser.Parse);
             var locations = lines.Select(parser.Parse).ToArray();
 
-            // DON'T FORGET TO LOG YOUR STEPS
+            // Competent coders log their steps. Be competent.
             // Grab the path from the name of your file
-
-
-
 
             // Now, here's the new code
 
@@ -73,20 +66,20 @@ namespace LoggingKata
             }
             logger.LogInfo($"The two locations furthest away from each other " +
                 $"are {tacobell1.Name} and {tacobell2.Name}, with a distance of {parser.ConvertMetersToMiles(distance)} miles.");
-            //DONE Create a new corA Coordinate with your locA's lat and long
+            // Create a new corA Coordinate with your locA's lat and long
 
-            //DONE Now, do another loop on the locations with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
+            // Now, do another loop on the locations with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
 
-            //DONE Create a new Coordinate with your locB's lat and long
+            // Create a new Coordinate with your locB's lat and long
 
-            //DONE Now, compare the two using `.GetDistanceTo()`, which returns a double
-            //DONE If the distance is greater than the currently saved distance, update the distance and the two `ITrackable` variables you set above
+            // Now, compare the two using `.GetDistanceTo()`, which returns a double
+            // If the distance is greater than the currently saved distance, update the distance and the two `ITrackable` variables you set above
 
-            //DONE Once you've looped through everything, you've found the two Taco Bells furthest away from each other.
+            // Once you've looped through everything, you've found the two Taco Bells furthest away from each other.
 
             /* Convert the distance to miles. Round to 2 decimal places.
-            Create a method to convert metres to miles in the TacoParser class
-            Write a unit test for the method in your tests
+            Create a method to convert meters to miles in the TacoParser class.
+            Write a unit test for the method in your tests to catch exceptions.
             Implement method in Program.cs */
             // Conversion: "var distanceInMiles = distanceInMetres / 1609;"
 
